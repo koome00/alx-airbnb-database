f24bc1394a1e9d460bedede46f51ab5804854e1d
koome <koomemc@gmail.com>
2024-12-02 21:32:35 +0300

    fix: order by add

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -6,7 +6,10 @@ INNER JOIN User u ON b.user_id = u.user_id;
 
 SELECT p.property_id, p.name, p.location, p.pricepernight, r.review_id, r.rating, r.comment
 FROM Property p
-LEFT JOIN Review r ON p.property_id = r.property_id;
+LEFT JOIN Review r ON p.property_id = r.property_id
+ORDER BY p.name; 
+
+
 
 
 SELECT u.user_id, u.first_name, u.last_name, u.email, b.booking_id, b.start_date, b.end_date, b.total_price, b.status
