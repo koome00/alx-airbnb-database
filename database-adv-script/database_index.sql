765c382616e8dd51d5fcc4f804b15ff172ea1d24
koome <koomemc@gmail.com>
2024-12-02 21:42:37 +0300

    fix: explain analyze

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -16,7 +16,16 @@ CREATE INDEX idx_property_location ON Property (location);
 CREATE INDEX idx_property_price ON Property (pricepernight);
 
 
-EXPLAIN SELECT * FROM Booking b 
+EXPLAIN ANALYZE 
+SELECT * 
+FROM Booking b 
+JOIN Property p ON b.property_id = p.property_id
+WHERE p.location = 'New York'
+ORDER BY b.start_date;
+
+EXPLAIN ANALYZE 
+SELECT * 
+FROM Booking b 
 JOIN Property p ON b.property_id = p.property_id
 WHERE p.location = 'New York'
 ORDER BY b.start_date;
