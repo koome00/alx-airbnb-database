d197f3503a73aebeeeebac2bf54b1f311c157e1e
koome <koomemc@gmail.com>
2024-12-02 21:35:32 +0300

    fix: use row_number

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -4,11 +4,12 @@ SELECT
     u.last_name,
     COUNT(b.booking_id) AS total_bookings
 FROM
-    User u
+    "User" u
 LEFT JOIN
     Booking b ON u.user_id = b.user_id
 GROUP BY
     u.user_id;
+
 
 
 SELECT
@@ -16,7 +17,7 @@ SELECT
     p.name,
     p.location,
     COUNT(b.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_row_number
 FROM
     Property p
 LEFT JOIN
@@ -24,4 +25,4 @@ LEFT JOIN
 GROUP BY
     p.property_id
 ORDER BY
-    booking_rank;
+    booking_row_number;
